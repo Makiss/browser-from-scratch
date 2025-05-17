d9570359205c64d0d5904d704dc356ead8ab5818
Maksym Somushkin <syomushkin.maxim@gmail.com>
2025-05-17 23:36:36 +0200

    feat: handle entities

--- main.py
+++ main.py
@@ -2,13 +2,25 @@ from src.url import URL
 
 def show(body: str):
     in_tag = False
-    for c in body:
-        if c == "<":
+    i = 0
+    while i < len(body):
+        if body[i:i+4] == "&lt;":
+            print("<", end="")
+            i += 4
+        elif body[i:i+4] == "&gt;":
+            print(">", end="")
+            i += 4
+        elif body[i] == "<":
             in_tag = True
-        elif c == ">":
+            i += 1
+        elif body[i] == ">":
             in_tag = False
+            i += 1
         elif not in_tag:
-            print(c, end="")
+            print(body[i], end="")
+            i += 1
+        else:
+            i += 1
 
 def load(url: URL):
     body = url.request()
